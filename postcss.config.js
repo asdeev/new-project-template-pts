6b08716583cd127d0b734c7fd675ecdd15af154f
Zaid Bhura <3915682+asdeev@users.noreply.github.com>
2021-01-24 15:55:27 -0800

    Initial setup

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-modules'),
+    purgecss({
+      content: ['./**/*.html'],
+    }),
+  ],
+};
